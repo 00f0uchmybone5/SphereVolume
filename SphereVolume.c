b9062fb3e570cca8f560f25af7304308174cbe50
Mr. Bones <satperson@mailfence.com>
2023-09-02 13:41:03 +0200

    First working commit.

--- SphereVolume.c
+++ SphereVolume.c
@@ -1,14 +1,17 @@
+#include <math.h>
 #include <stdio.h>
+
 
 main()
 {
-    float pi = 3.14, radius, volume;
+    double pi = M_PI;
+    float radius, volume;
     printf("This program will calculate the volume of your sphere.\n");
     printf("Enter your radius: ");
     scanf("%f", &radius);
 
-    volume = 4 / 3 * pi * radius ^ 3;
+    volume = (4.0 / 3.0) * pi * radius * radius * radius;
 
-    printf("The volume of your sphere is: ", volume);
+    printf("The volume of your sphere is: %.1f\n", volume);
     return 0;
 }
